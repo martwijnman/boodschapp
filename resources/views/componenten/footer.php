bbc92cde570798c7380585882fa94201c214cd75
Inetjuhhh <35923627+Inetjuhhh@users.noreply.github.com>
2024-02-26 12:12:39 +0100

    restore app

---
+++ resources/views/componenten/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="wrapper">
+        <p>&copy; Ine Vermeer, 2024</p>
+    </div>
+</footer>
---
+++ resources/views/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php require_once 'componenten/head.php'; ?>
+	<title>BoodschApp</title>
+	<link rel="stylesheet" href="../../public_html/css/style.css">
+</head>
+<body>
+	<?php require_once 'componenten/header.php'; ?>
+	<main>
+		<div class="wrapper frontpage">
+			<h2>Welkom bij de boodschappen app</h2>
+			<p>Log in en klik op 'Jouw boodschappen' om je boodschappenlijst te zien.</p>
+			<img src="../../public_html/img/boodschappenbezorgen.png" alt="">
+
+		</div>
+	</main>
+	<?php require_once __DIR__.'/componenten/footer.php'; ?>
+
+
+</body>
+</html>
